8a54721435c96f279918e106e24e54176a398e96
lazybones <39758771+yllwin94@users.noreply.github.com>
2024-11-07 17:30:16 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class BookController {
+
+    /**
+     * 访问主页
+     * @param model
+     * @return
+     */
+    @GetMapping("/index")
+    public String index(Model model){
+        model.addAttribute("tip", "first springboot project");
+        return "hello";
+    }
+
+    @GetMapping("/book")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> book(Model model){
+        return new ResponseEntity<>(Map.of("name","lily's book", "price", "100"), HttpStatus.OK);
+    }
+
+}
